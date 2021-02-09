2d608a913d30fcc9168608720ac03894f2812490
ramiksaier <ramos.ksaier@gmail.com>
2021-02-09 23:09:04 +0100

    Appdddd

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './component/navbar'
+import Bonjour from '../src/component/Listprod'
+import Footer from './component/footer'
+function App() {
+  return (
+    <div className="App">
+
+     <Navbar/>
+<Bonjour/>
+<Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ component/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Navbar, Nav, Form, FormControl,  } from 'react-bootstrap'
+
+
+const Nabar = () => {
+    return (
+        <div>
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="#home">About MOvies </Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home"></Nav.Link>
+                    <Nav.Link href="#features">movies</Nav.Link>
+                    <Nav.Link href="#pricing">movies</Nav.Link>
+                </Nav>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-info">Search</Button>
+                </Form>
+            </Navbar>
+        </div>
+    )
+}
+
+export default Nabar
